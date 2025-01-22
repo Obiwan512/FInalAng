68889e9365efefa4af8c85276458ae0cbbd70be7
LAB321 Student18 <lab321Student18@gau.local>
2025-01-22 21:09:09 +0400

    Final

--- Final/src/app/tamplate/login/login.component.ts
+++ Final/src/app/tamplate/login/login.component.ts
@@ -38,7 +38,7 @@ export class LoginComponent {
             this.router.navigate(['/home-page']);
           }
         },
-        (error) => {
+        (error:any) => {
           console.error('Login failed', error);
           alert('Login failed: ' + error.message);
         }
